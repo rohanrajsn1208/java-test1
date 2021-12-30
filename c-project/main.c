4618fbc3021ad29d8045898a91cea7cec8313e3a
Cloud User <ec2-user@ip-172-31-16-156.us-east-2.compute.internal>
2021-12-30 10:34:30 +0000

    compiled source code

---
+++ c-project/main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void main(){
+
+	big2();
+printf("\n");
+	big3();
+printf("\n");
+
+	fact();
+printf("\n");
+
+	rev();
+printf("\n");
+
+	palindrome();
+printf("\n");
+
+	sumofn();
+printf("\n");
+
+	fibonacci();
+printf("\n");
+
+	sort();
+printf("\n");
+
+}
+
